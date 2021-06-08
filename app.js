b8105956034b598bdd5fe103a78ef488e4e8f9f3
qui <quinguyen73@gmail.com>
2021-06-08 01:08:34 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from qvnguye!'));
+app.get('/', (req, res) => res.send('Hello World from qvnguye for review stage!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
